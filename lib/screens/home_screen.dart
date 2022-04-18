0e498dcd17bec7bf310916db0fe4735171d71980
madison08 <madisonamoa@gmail.com>
2022-04-18 17:51:28 +0000

    start project

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('hello'),
+      ),
+    );
+  }
+}
